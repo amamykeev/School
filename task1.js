2b14095300e3eefcb9a3da5f51d94b5bd5b08a87
azamat <amamykeev>
2019-06-18 14:46:56 +0600

    'done'

---
+++ task1.js
@@ -0,0 +1,21 @@
+class Person {
+    constructor(name, contact, isActive, dateOfBirth) {
+        this.name = name;
+        this.contact = contact;
+        this.isActive = isActive;
+        this.dateOfBirth = dateOfBirth;
+    }
+    setName() {
+        setName = name;
+    }
+    setContact() {
+        setcontact = this.contact;
+    }
+    setActive() {
+        setActive = this.isActive;
+    }
+    setDate() {
+        setDate = this.dateOfBirth;
+    }
+}
+
