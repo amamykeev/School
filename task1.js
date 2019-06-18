028516df0f50f06f1ff9c2b3f0945ad0185cd206
azamat <amamykeev>
2019-06-18 17:55:05 +0600

    'fixed'

--- task1.js
+++ task1.js
@@ -1,21 +1,148 @@
 class Person {
-    constructor(name, contact, isActive, dateOfBirth) {
-        this.name = name;
-        this.contact = contact;
-        this.isActive = isActive;
-        this.dateOfBirth = dateOfBirth;
-    }
-    setName() {
-        setName = name;
-    }
-    setContact() {
-        setcontact = this.contact;
-    }
-    setActive() {
-        setActive = this.isActive;
-    }
-    setDate() {
-        setDate = this.dateOfBirth;
-    }
+	constructor(name, contact, isActive, dateOfBirth) {
+			this.setName(name);
+			this.setContact(contact);
+			this.setActive(isActive);
+			this.setDate(dateOfBirth);
+	}
+	setName(name) {
+			this.name = name;
+	}
+	setContact(contact) {
+			this.contact = contact;
+	}
+	setActive(isActive) {
+			this.isActive = isActive;
+	}
+	setDate(dateOfBirth) {
+			this.dateOfBirth = dateOfBirth;
+	}
 }
 
+class Book {
+	constructor(name, subject) {
+		this.setName(name);
+		this.setSubject(subject);
+	}
+	setName(name) {
+		this.name = name;
+	}
+	setSubject(subject) {
+		this.subject = subject;
+	}
+}
+
+class Position {
+	constructor(name, quote) {
+		this.setName(name);
+		this.setQuote(quote);
+	}
+	setName(name) {
+		this.setName = name;
+	}
+	setQuote(quote) {
+		this.quote = quote;
+	}
+}
+
+class Subject {
+	constructor(name, level, hours, books = [], quote) {
+		this.setName(name);
+		this.setLevel(level);
+		this.setHours(hours);
+		this.setBooks(books);
+		this.setQuote(quote);
+	}
+	setName(name) {
+		this.name = name;
+	}
+	setLevel(level) {
+		this.level = level;
+	}
+	setHours(hours) {
+		this.hours = hours;
+	}
+	setBooks(books) {
+		this.books = books;
+	}
+	setQuote(quote) {
+		this.quote = quote;
+	}
+}
+
+class TeacherRole {
+	constructor(teacher, subject) {
+		this.setTeacher(teacher);
+		this.setSubject(subject);
+	}
+	setTeacher(teacher) {
+		this.teacher = teacher;
+	}
+	setSubject(subject) {
+		this.subject = subject;
+	}
+}
+
+class StudentGroup {
+	constructor(student = [], level, groupNumber, teacherRole) {
+		this.setStudent(student);
+		this.setLevel(level);
+		this.setGroupNumber(groupNumber);
+		this.setTeacherRole(teacherRole);
+	}
+	setStudent(student) {
+		this.student = student;
+	}
+	setLevel(level) {
+		this.level = level;
+	}
+	setGroupNumber(groupNumber) {
+		this.groupNumber = groupNumber;
+	}
+	setTeacherRole(teacherRole) {
+		this.teacherRole = teacherRole;
+	}
+}
+
+class Parent extends Person {
+	constructor(name, contact, jobPosition) {
+		super(name, contact);
+		this.setJobPosition(jobPosition)
+	}
+	setJobPosition(jobPosition) {
+		this.jobPosition = jobPosition
+	}
+}
+
+class Student extends Person {
+	constructor(name, contact, isActive, dateOfBirth, studentGroup, parent = []) {
+		super(name, contact, isActive, dateOfBirth, studentGroup, parent);
+	}
+	setStudentGroup(studentGroup) {
+		studentGroup = new studentGroup();
+	}
+	addParent(parent) {
+		this.parent.push(new parent)
+	}
+}
+
+
+class Teacher extends Person {
+	constructor(name, contact, isActive, dateOfBirth, subjects = [], studentGroup = [], salary, position) {
+		super(name, contact, isActive, dateOfBirth);
+		this.setSalary(salary);
+		this.setPosition(position);
+	}
+	setSalary(salary) {
+		this.salary = salary;
+	}
+	setPosition(position) {
+		this.position = new position;
+	}
+	addSubject(subject) {
+		this.subject.push(new subject);
+	}
+	addStudentGroup(studentGroup) {
+		this.studentGroup.push(new studentGroup(studentGroup));
+	}
+}
